a5daa305a1b2069c2cb1686d8571fcc22b8cffaa
pwh9882 <pwh9882@gmail.com>
2023-03-06 01:09:06 +0900

    code challenge: Transform을 CurrencyCard 에 옮기고 order를 입력받아 offset설정.

--- lib/main.dart
+++ lib/main.dart
@@ -111,33 +111,30 @@ class App extends StatelessWidget {
                     height: 20,
                   ),
                   Column(
-                    children: [
-                      const CurrencyCard(
+                    children: const [
+                      CurrencyCard(
                         name: 'Euro',
                         code: 'EUR',
                         amount: '6 428',
                         icon: Icons.euro_rounded,
                         isInverted: false,
+                        order: 1,
                       ),
-                      Transform.translate(
-                        offset: const Offset(0, -20),
-                        child: const CurrencyCard(
-                          name: 'Bitcoin',
-                          code: 'BTC',
-                          amount: '9 785',
-                          icon: Icons.currency_bitcoin,
-                          isInverted: true,
-                        ),
+                      CurrencyCard(
+                        name: 'Bitcoin',
+                        code: 'BTC',
+                        amount: '9 785',
+                        icon: Icons.currency_bitcoin,
+                        isInverted: true,
+                        order: 2,
                       ),
-                      Transform.translate(
-                        offset: const Offset(0, -40),
-                        child: const CurrencyCard(
-                          name: 'Dollar',
-                          code: '\$',
-                          amount: '9 785',
-                          icon: Icons.attach_money_outlined,
-                          isInverted: false,
-                        ),
+                      CurrencyCard(
+                        name: 'Dollar',
+                        code: '\$',
+                        amount: '9 785',
+                        icon: Icons.attach_money_outlined,
+                        isInverted: false,
+                        order: 3,
                       ),
                     ],
                   )
--- lib/widgets/currency_card.dart
+++ lib/widgets/currency_card.dart
@@ -5,6 +5,7 @@ class CurrencyCard extends StatelessWidget {
   final IconData icon;
   final bool isInverted;
   final Color _blackColor = const Color(0xff1f2123);
+  final int order;
 
   const CurrencyCard({
     super.key,
@@ -13,76 +14,80 @@ class CurrencyCard extends StatelessWidget {
     required this.amount,
     required this.icon,
     required this.isInverted,
+    required this.order,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-          color: isInverted ? Colors.white : _blackColor,
-          borderRadius: BorderRadius.circular(25)),
-      padding: const EdgeInsets.symmetric(
-        vertical: 10,
-        horizontal: 10,
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.w600,
+    return Transform.translate(
+      offset: Offset(0, (order - 1) * -20),
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+            color: isInverted ? Colors.white : _blackColor,
+            borderRadius: BorderRadius.circular(25)),
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 10,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w600,
+                      color: isInverted ? _blackColor : Colors.white,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        amount,
+                        style: TextStyle(
+                          color: isInverted ? _blackColor : Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        code,
+                        style: TextStyle(
+                          color: isInverted
+                              ? _blackColor.withOpacity(0.7)
+                              : Colors.white.withOpacity(0.7),
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              Transform.translate(
+                offset: const Offset(-5, 25),
+                child: Transform.scale(
+                  scale: 2.2,
+                  child: Icon(
+                    icon,
                     color: isInverted ? _blackColor : Colors.white,
+                    size: 88,
                   ),
                 ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      amount,
-                      style: TextStyle(
-                        color: isInverted ? _blackColor : Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      code,
-                      style: TextStyle(
-                        color: isInverted
-                            ? _blackColor.withOpacity(0.7)
-                            : Colors.white.withOpacity(0.7),
-                        fontSize: 16,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            Transform.translate(
-              offset: const Offset(-5, 25),
-              child: Transform.scale(
-                scale: 2.2,
-                child: Icon(
-                  icon,
-                  color: isInverted ? _blackColor : Colors.white,
-                  size: 88,
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
