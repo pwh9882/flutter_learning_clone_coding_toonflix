0fb04f03e4f7df8db31de65508f1b1b96d31dab6
pwh9882 <pwh9882@gmail.com>
2023-03-11 01:02:49 +0900

    ready for leaning BuildContext

--- lib/main.dart
+++ lib/main.dart
@@ -10,46 +10,13 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int counter = 0;
-  List<int> numbers = [];
-
-  void onClicked() {
-    setState(() {
-      numbers.add(counter++);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
-          backgroundColor: const Color(0xFFF4EDDB),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  "Total clicked",
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-                Text(
-                  '$counter',
-                  style: const TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-                // for (var i = 0; i < numbers.length; i++) Text('${numbers[i]}'),
-                for (var number in numbers) Text('$number'),
-                IconButton(
-                  onPressed: onClicked,
-                  iconSize: 40,
-                  icon: const Icon(Icons.add_box_outlined),
-                )
-              ],
-            ),
-          )),
+        backgroundColor: Color(0xFFF4EDDB),
+        body: Center(),
+      ),
     );
   }
 }
