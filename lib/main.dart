e985afe8a34298bcc851bebc23564ff8d94c61aa
pwh9882 <pwh9882@gmail.com>
2023-03-11 15:21:30 +0900

    ready for foonflix coding

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_learning_clone_coding_toonflix/screens/homeScreen.dart';
 
 void main() {
   runApp(App());
@@ -9,15 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        backgroundColor: const Color(0xffe7626c),
-        textTheme: const TextTheme(
-            headline1: TextStyle(
-          color: Color(0xFF232B55),
-        )),
-        cardColor: const Color((0xFFF4EDDB)),
-      ),
-      home: const HomeScreen(),
+      home: Container(),
     );
   }
 }
