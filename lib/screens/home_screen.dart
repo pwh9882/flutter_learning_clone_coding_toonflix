e762c6dd90074bbdf211fb3500ef84c80ed5cd49
pwh9882 <pwh9882@gmail.com>
2023-03-11 16:39:53 +0900

    simple way to load api data(Not much recommended)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learning_clone_coding_toonflix/models/webtoon_model.dart';
+import 'package:flutter_learning_clone_coding_toonflix/services/api_service.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<WebtoonModel> webtoons = [];
+  bool isLoading = true;
+
+  void waitForWebtoons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    waitForWebtoons();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
