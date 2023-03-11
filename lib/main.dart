8405267f5bd151d7f4e7189ed23e5808db3909c8
pwh9882 <pwh9882@gmail.com>
2023-03-11 12:05:36 +0900

    ready for pomodoro app clone

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_learning_clone_coding_toonflix/screens/homeScreen.dart';
 
 void main() {
   runApp(App());
 }
 
-class App extends StatefulWidget {
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
-
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          textTheme: const TextTheme(
-        titleLarge: TextStyle(
-          color: Colors.red,
-          fontSize: 40,
-        ),
-      )),
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? const MyLargeTitle() : const Text("Nothing to see"),
-              IconButton(
-                onPressed: toggleTitle,
-                icon: Icon(
-                  showTitle ? (Icons.toggle_off) : Icons.toggle_on,
-                  size: 50,
-                ),
-              )
-            ],
-          ),
+        backgroundColor: const Color(0xffe7626c),
+        textTheme: const TextTheme(
+            headline1: TextStyle(
+          color: Color(0xFF232B55),
+        )),
+        cardTheme: const CardTheme(
+          color: Color(0xFFF4EDDB),
         ),
       ),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    super.initState();
-    print("Hello World");
-  }
-
-  @override
-  void dispose() {
-    print('dispose');
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('object');
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
+      home: const HomeScreen(),
     );
   }
 }
