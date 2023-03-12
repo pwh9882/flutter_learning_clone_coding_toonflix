f4f6d25c5703d8c6acb9b4567ab5ff413bf8621b
pwh9882 <pwh9882@gmail.com>
2023-03-12 20:27:24 +0900

    favorite 기능

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -3,8 +3,9 @@ import 'package:flutter_learning_clone_coding_toonflix/models/webtoon_detail_mod
 import 'package:flutter_learning_clone_coding_toonflix/models/webtoon_episode_model.dart';
 import 'package:flutter_learning_clone_coding_toonflix/models/webtoon_model.dart';
 import 'package:flutter_learning_clone_coding_toonflix/widgets/webtoon_episode.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final WebtoonModel webtoon;
   Future<WebtoonDetailModel> webtoonDetail;
   Future<List<WebtoonEpisodModel>> webtoonEpisodes;
@@ -17,16 +18,63 @@ class DetailScreen extends StatelessWidget {
   });
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late SharedPreferences preferences;
+
+  bool isLiked = false;
+
+  Future initPref() async {
+    preferences = await SharedPreferences.getInstance();
+    final likedToons = preferences.getStringList("likedToons");
+    if (likedToons != null) {
+      if (likedToons.contains(widget.webtoon.id) == true) {
+        setState(() {
+          isLiked = true;
+        });
+        // print("liked!");
+      }
+    } else {
+      await preferences.setStringList('likedToons', []);
+    }
+  }
+
+  void onHeartTap() async {
+    final likedToons = preferences.getStringList('likedToons');
+    if (likedToons != null) {
+      if (isLiked) {
+        likedToons.remove(widget.webtoon.id);
+      } else {
+        likedToons.add(widget.webtoon.id);
+      }
+      await preferences.setStringList('likedToons', likedToons);
+    }
+    isLiked = !isLiked;
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
+    initPref();
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: onHeartTap,
+            icon: isLiked
+                ? const Icon(Icons.favorite_outlined)
+                : const Icon(Icons.favorite_outline_outlined),
+          ),
+        ],
         centerTitle: true,
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         elevation: 2,
         title: Text(
-          webtoon.title,
+          widget.webtoon.title,
           style: const TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w600,
@@ -46,7 +94,7 @@ class DetailScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Hero(
-                    tag: webtoon.id,
+                    tag: widget.webtoon.id,
                     child: Container(
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(15),
@@ -60,7 +108,7 @@ class DetailScreen extends StatelessWidget {
                       clipBehavior: Clip.hardEdge,
                       width: 250,
                       child: Image.network(
-                        webtoon.thumbnail,
+                        widget.webtoon.thumbnail,
                         headers: const {
                           "User-Agent":
                               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
@@ -74,7 +122,7 @@ class DetailScreen extends StatelessWidget {
                 height: 25,
               ),
               FutureBuilder(
-                future: webtoonDetail,
+                future: widget.webtoonDetail,
                 builder: ((context, snapshot) {
                   if (snapshot.hasData) {
                     return Column(
@@ -103,7 +151,7 @@ class DetailScreen extends StatelessWidget {
                 height: 50,
               ),
               FutureBuilder(
-                future: webtoonEpisodes,
+                future: widget.webtoonEpisodes,
                 builder: ((context, snapshot) {
                   if (snapshot.hasData) {
                     return Column(
@@ -111,7 +159,7 @@ class DetailScreen extends StatelessWidget {
                         for (var episode in snapshot.data!)
                           WebtoonEpisode(
                             episode: episode,
-                            webtoonId: webtoon.id,
+                            webtoonId: widget.webtoon.id,
                           )
                       ],
                     );
