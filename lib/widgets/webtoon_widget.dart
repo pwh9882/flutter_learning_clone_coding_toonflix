22e2d456542b8f13bd43a7905edf48fd1318cadf
pwh9882 <pwh9882@gmail.com>
2023-03-12 17:42:41 +0900

    statefull를 활용하고 싶으면 State 쪽에서 late로 선언하고 initState에서 로딩해오면됨. 필드 접근은 state.id같이.

--- lib/widgets/webtoon_widget.dart
+++ lib/widgets/webtoon_widget.dart
@@ -4,6 +4,9 @@ import 'package:flutter_learning_clone_coding_toonflix/models/webtoon_model.dart
 import 'package:flutter_learning_clone_coding_toonflix/screens/detail_screen.dart';
 import 'package:flutter_learning_clone_coding_toonflix/services/api_service.dart';
 
+// 만약 데이터 로딩이 빈번하고, 이를 위젯에서 로딩하고 싶을 때의 코드 예시:
+// https://github.com/nomadcoders/toonflix/commit/b2f44bf9b0cf7fce32c52dcefddc254ab1b8a290
+
 class Webtoon extends StatelessWidget {
   final WebtoonModel webtoon;
 
